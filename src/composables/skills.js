5f03f70cbe0b0330f61ac0b447adcce778f710e8
unknown <mutungaeric839@gmail.com>
2023-05-06 11:27:01 +0300

    Add axios and import ref function from vue module

--- src/composables/skills.js
+++ src/composables/skills.js
@@ -1,4 +1,15 @@
+import { ref } from "vue";// import the ref function from the vue module.
+//ref is a built-in function that allows you to create reactive variables. When a variable is defined as a ref, any changes made to its value are automatically tracked by Vue.js, and any components that use that variable will be updated to reflect the new value.
+//
 export default function useSkills() 
 {
+    const skills = ref([]);
+    const skill = ref([]);
+    const getSkills = async () => {
 
+    }
+    
+    return{
+
+    }
 }
